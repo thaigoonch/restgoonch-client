b963d74562919e1c4550bfe4f958c498bc0f2cbc
Katherine Nieman <thaigoonch@gmail.com>
2022-05-13 15:03:35 -0600

    REST-erize

--- cmd/restgoonchclient/main.go
+++ cmd/restgoonchclient/main.go
@@ -1,50 +1,27 @@
 package main
 
 import (
-	"fmt"
+	"bytes"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"sync"
 
+	"github.com/golang/protobuf/proto"
 	restgoonch "github.com/thaigoonch/restgoonch/service"
-	"golang.org/x/net/context"
-)
-
-var (
-	port     = 8080
-	promPort = 9099
 )
 
 func main() {
-	// Create an http server for prometheus
-	httpServer := &http.Server{
-		Handler: promhttp.HandlerFor(reg, promhttp.HandlerOpts{}),
-		Addr:    fmt.Sprintf(":%d", promPort)}
-
-	// Start http server for prometheus
-	go func() {
-		if err := httpServer.ListenAndServe(); err != nil {
-			log.Fatalf("Error starting http server: %v", err)
-		}
-	}()
-
-	host := "grpcgoonch-nonheadless-service"
-	opts := []grpc.DialOption{
-		grpc.WithTransportCredentials(insecure.NewCredentials()),
-		grpc.WithUnaryInterceptor(grpcMetrics.UnaryClientInterceptor()),
-	}
-	conn, err := grpc.Dial(fmt.Sprintf("%s:%d", host, port), opts...)
-	if err != nil {
-		grpclog.Fatalf("Could not connect on port %d: %v", port, err)
-	}
-	defer conn.Close()
-
-	c := grpcgoonch.NewServiceClient(conn)
 	text := "encrypt me"
 	key := []byte("#89er@jdks$jmf_d")
-	request := grpcgoonch.Request{
+	request := &restgoonch.Request{
 		Text: text,
 		Key:  key,
+	}
+
+	req, err := proto.Marshal(request)
+	if err != nil {
+		log.Fatalf("Unable to marshal request : %v", err)
 	}
 
 	wg := sync.WaitGroup{}
@@ -52,12 +29,18 @@ func main() {
 		wg.Add(1)
 		go func() {
 			for i := 0; i < 200; i++ {
-				response, err := c.CryptoRequest(context.Background(), &request)
+				resp, err := http.Post("http://0.0.0.0:8080", "application/x-binary", bytes.NewReader(req))
 				if err != nil {
-					grpclog.Fatalf("Error when calling CryptoRequest(): %v", err)
+					log.Fatalf("Unable to read from the server : %v", err)
 				}
+				respBytes, err := ioutil.ReadAll(resp.Body)
 
-				log.Printf("Response from Goonch Server: %s", response.Result)
+				if err != nil {
+					log.Fatalf("Unable to read bytes from request : %v", err)
+				}
+				respObj := &restgoonch.DecryptedText{}
+				proto.Unmarshal(respBytes, respObj)
+				log.Printf("Response from Goonch Server: %s", respObj.GetResult())
 			}
 			wg.Done()
 		}()
--- go.mod
+++ go.mod
@@ -3,24 +3,7 @@ module github.com/thaigoonch/restgoonch-client
 go 1.17
 
 require (
-	github.com/grpc-ecosystem/go-grpc-prometheus v1.2.0
-	github.com/prometheus/client_golang v1.12.1
-	golang.org/x/net v0.0.0-20220425223048-2871e0cb64e4
-	google.golang.org/grpc v1.46.0
-)
-
-require (
-	github.com/beorn7/perks v1.0.1 // indirect
-	github.com/cespare/xxhash/v2 v2.1.2 // indirect
-	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
-	github.com/prometheus/client_model v0.2.0 // indirect
-	github.com/prometheus/common v0.34.0 // indirect
-	github.com/prometheus/procfs v0.7.3 // indirect
-	github.com/thaigoonch/grpcgoonch-nonheadless v0.0.0-20220504063030-3de96144b250
+	github.com/golang/protobuf v1.5.2
 	github.com/thaigoonch/restgoonch v0.0.0-20220513203656-7ebb885c0ff2
-	golang.org/x/sys v0.0.0-20220503163025-988cb79eb6c6 // indirect
-	golang.org/x/text v0.3.7 // indirect
-	google.golang.org/genproto v0.0.0-20220503193339-ba3ae3f07e29 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
 )
